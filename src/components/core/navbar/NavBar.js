cf1e34e5f5368fa23329c0bb20b6fe5df9e5aa8a
Eformeh-Bello Wisdom <oyievesho50@gmail.com>
2021-06-09 17:46:12 +0100

    update changes on landing and modal register

--- src/components/core/navbar/NavBar.js
+++ src/components/core/navbar/NavBar.js
@@ -1,8 +1,21 @@
 import { faSuperpowers } from '@fortawesome/free-brands-svg-icons';
-import React from 'react';
-import { Button, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Nav, Navbar, NavDropdown, Modal, Table, Form } from 'react-bootstrap';
 import logo from '../../../assets/theHubLogo.svg';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 export default function NavBar() {
+
+    const [loginShow, setLoginShow] = useState(false);
+    const [registerShow, setRegisterShow] = useState(false);
+    const handleLoginClose = () => setLoginShow(false);
+    const handleLoginShow = () => setLoginShow(true);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+    const handleRegisterClose = () => setRegisterShow(false);
+    const handleRegisterShow = () => setRegisterShow(true);
+    // please also note that the login and register page are inside navbar due to the fact that i use modal bootstrap
     return (
         <div>
 
@@ -22,12 +35,185 @@ export default function NavBar() {
                             <NavDropdown.Item href="#testimonials">Testimonials</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
+                    {/* this login and register button are linked to a modal popup that opens the login and regist overlay */}
                     <Nav className="ml-auto">
-                        <Nav.Link href="#login">login</Nav.Link>
-                        <Nav.Link href="#register"><Button className="btn-sm" bg="dark" variant="outline-dark">Register</Button></Nav.Link>
+                        <Nav.Link onClick={handleLoginShow}>login</Nav.Link>
+                        <Nav.Link onClick={handleRegisterShow}><Button className="btn-sm" bg="dark" variant="outline-dark">Register</Button></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
+            {/* this first modal is the login overlay */}
+            <Modal
+                show={loginShow}
+                onHide={handleLoginClose}
+                keyboard={false}
+                aria-labelledby="example-modal-sizes-title-login"
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="example-modal-sizes-title-login">
+                        Login
+          </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Formik
+                        initialValues={
+                            {
+                                email: '',
+                                password: '',
+                            }}
+                        validationSchema={Yup.object({
+                            email: Yup.string()
+                                .email('invalid Email address')
+                                .required('Required'),
+                            password: Yup.string()
+                                .required("Please provide your password.")
+                                .min(8, "Password is too short - should be 8 chars minimum.")
+                                .matches(/(?=.*[0-9])/, "Password must contain a number."),
+
+                        })}
+                    >
+                        <Form onSubmit={handleSubmit}   >
+                            <Form.Group controlId="emailLogin">
+                                <Form.Control name="email" type="email" placeholder="Email Address" />
+                            </Form.Group>
+                            <Form.Group width='20px' controlId="passwordLogin">
+                                <Form.Control name="password" type="password" placeholder="Password" />
+                            </Form.Group>
+
+                        </Form>
+
+                    </Formik>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary">Close</Button>
+                    <Button className="btn-lg" variant="primary"  type="submit">
+                        Sign In </Button>
+                </Modal.Footer>
+            </Modal>
+
+            {/* this second modal is the register overlay */ }
+    <Modal
+        show={registerShow}
+        onHide={handleRegisterClose}
+        keyboard={false}
+        aria-labelledby="example-modal-sizes-title-register"
+    >
+        <Modal.Header closeButton>
+            <Modal.Title id="example-modal-sizes-title-register">
+                Register
+                </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Formik
+                initialValues={
+                    {
+                        firstName: '',
+                        LastName: '',
+                        email: '',
+                        password: '',
+                        confirmPassword: '',
+                        gender: '',
+                        phone: '',
+                        referral: '',
+                        yearExp: '',
+                        acceptedTerms: false
+                    }}
+                validationSchema={Yup.object({
+                    firstName: Yup.string()
+                        .min(3, 'must be atleast 3 characters')
+                        .max(15, 'must be atleast 15 characters of less')
+                        .required('Required'),
+                    lastName: Yup.string()
+                        .min(3, 'must be atleast 3 characters')
+                        .max(15, 'must be atleast 15 characters of less')
+                        .required('Required'),
+                    email: Yup.string()
+                        .email('invalid Email address')
+                        .required('Required'),
+                    password: Yup.string()
+                        .required("Please provide your password.")
+                        .min(8, "Password is too short - should be 8 chars minimum.")
+                        .matches(/(?=.*[0-9])/, "Password must contain a number."),
+                    confirmPassword: Yup.string()
+                        .oneOf([Yup.ref('password'), null], "Passwords don't match!")
+                        .required('Required'),
+                    phone: Yup.number()
+                        .typeError("That doesn't look like a phone number")
+                        .positive("A phone number can't start with a minus")
+                        .integer("A phone number can't include a decimal point")
+                        .min(8)
+                        .max(14)
+                        .required('A phone number is required'),
+
+                    gender: Yup.string()
+                        .oneOf(['male', 'female'], 'please choose a gender')
+                        .required(),
+                    yearExp: Yup.string()
+                        .oneOf(['0-1 years', '1-2 years', '2-3 years', '3-4 years', '4-5 years', '5-6 years', '6-7 years', '7-8 years', '8-9 years', '9-10 years'], 'please choose a gender')
+                        .required(),
+                    acceptedTerms: Yup.boolean()
+                        .required()
+                        .oneOf([true], 'You must accept the terms and conditions'),
+                })}
+                onSubmit={(values, { setSubmitting, resetForm }) => {
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        resetForm();
+                        setSubmitting(false);
+                    }, 3000)
+                }}
+            >
+                <Form>
+                    <Form.Group controlId="email">
+                        <Form.Label>Email address*</Form.Label>
+                        <Form.Control type="email" placeholder="name@example.com" />
+                    </Form.Group>
+                    <Form.Group controlId="password">
+                        <Form.Label>Password*</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="ConfirmPassword">
+                        <Form.Label> Confirm Password*</Form.Label>
+                        <Form.Control type="password" placeholder=" Confirm Password" />
+                    </Form.Group>
+                    <Form.Group controlId="Gender">
+                        <Form.Control as="select" name="gender" label="Gender*">
+                            <option>Male</option>
+                            <option>Female</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="yearExp">
+                        <Form.Label>year(s) of Experience?*</Form.Label>
+                        <Form.Control as="select">
+                            <option>0-1 years</option>
+                            <option>1-2 years</option>
+                            <option>2-3 years</option>
+                            <option>3-4 years</option>
+                            <option>4-5 years</option>
+                            <option>5-6 years</option>
+                            <option>6-7 years</option>
+                            <option>7-8 years</option>
+                            <option>8-9 years</option>
+                            <option>9-10 years</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="ConfirmPassword">
+
+                        <Form.Label size="15em"> i accept the terms and conditions that apply<Form.Control type="checkbox" /></Form.Label>
+
+                    </Form.Group>
+
+                </Form>
+            </Formik>
+
+        </Modal.Body>
+        <Modal.Footer>
+            <Button className="btn-lg" variant="primary" type="submit">
+                Sign Up </Button>
+        </Modal.Footer>
+    </Modal>
+
+
         </div >
     )
 }
--- src/components/pages/landing/landing.js
+++ src/components/pages/landing/landing.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import { Button, Col, Image, Jumbotron, Row } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button, Col, Image, Jumbotron, Row, Card, Modal, Table } from 'react-bootstrap'
 import './landing.css';
+import { Formik, Field, Form, ErrorMessage, FieldArray } from 'formik';
 
 export default function Landing() {
+    // these are for toggling the modal popup
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <div>
+
             <div id="body">
                 <Row>
                     <Col>
@@ -12,7 +19,7 @@ export default function Landing() {
                             <h1><b>The better way to find <br />your next role!</b></h1>
                             <p>Chat, interview and get hired from any mobile device, tablet or computer.<br />Connect with companies looking for top black tech talent.</p>
                             <h1 >
-                                <Button className="btn-lg" variant="outline-success">Apply</Button>
+                                <Button className="btn-lg" variant="outline-dark">Apply</Button>
                                 <Button className="btn-lg" variant="outline-primary">Hire Top Talent</Button>
                             </h1>
                         </Jumbotron>
@@ -30,7 +37,7 @@ export default function Landing() {
                             <h1><b>Get matched to <br />tech teams based <br />on your skills</b></h1>
                             <p>On eWorker, companies are hiring software developers, devOps engineers<br /> and data scientist just like you. Complete your profile, get vetted and <br />start applying for your next remote role.</p>
                             <h1 >
-                                <Button className="btn-lg" variant="outline-success">Apply</Button>
+                                <Button className="btn-lg" variant="outline-dark">Apply</Button>
                             </h1>
                         </Jumbotron>
                     </Col>
@@ -50,7 +57,7 @@ export default function Landing() {
 
                                 <b>Complete our Screening</b>
 
-                                Pass our technical screening processes and interviews with your dedicated talent manager – who will ensure you receive the best offers.
+                                <p>Pass our technical screening processes and interviews with your dedicated talent manager – who will ensure you receive the best offers.</p>
 
                                 <b>Get Visible and Receive Offers</b>
 
@@ -63,7 +70,7 @@ export default function Landing() {
 
                             </p>
                             <h1 >
-                                <Button className="btn-lg" variant="outline-success">Apply</Button>
+                                <Button className="btn-lg" variant="outline-dark">Apply</Button>
                             </h1>
                         </Jumbotron>
                     </Col>
@@ -80,7 +87,7 @@ export default function Landing() {
                             <h1><b>Work with your favorite<br /> tech stack</b></h1>
                             <p>Whether it’s Python, Scala, JavaScript or anything else, on eWorker you only <br />get offers from companies for your preferred technology stack and desired salary.<br /> – helping you to find jobs up to 50% faster.</p>
                             <h1 >
-                                <Button className="btn-lg" variant="outline-success">Apply</Button>
+                                <Button className="btn-lg" variant="outline-dark">Apply</Button>
                             </h1>
                         </Jumbotron>
                     </Col>
@@ -88,6 +95,7 @@ export default function Landing() {
                 </Row>
 
             </div>
+
         </div>
     )
 }
